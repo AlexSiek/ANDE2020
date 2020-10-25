bac0d7ae029e41a31dc425d82f19b14eddceaf28
Alex Siek <alexsiekminping@gmail.com>
2020-10-25 16:57:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ande/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ande;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.PopupMenu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
