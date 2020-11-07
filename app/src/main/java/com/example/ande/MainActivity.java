a1b38a6317af036fda644f2139524bc10a3f41e4
Alex Siek <alexsiekminping@gmail.com>
2020-11-07 23:01:06 +0800

    Added popup menu in main layout

--- app/src/main/java/com/example/ande/MainActivity.java
+++ app/src/main/java/com/example/ande/MainActivity.java
@@ -1,19 +1,42 @@
 package com.example.ande;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.PopupMenu;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton btn = (ImageButton) findViewById(R.id.popup_menu);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, v);
+                popup.setOnMenuItemClickListener(MainActivity.this);
+                popup.inflate(R.menu.popup_menu);
+                popup.show();
+            }
+        });
     }
 
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.setting_item:
+                // do your code
+                return true;
+            case R.id.about_item:
+                // do your code
+                return true;
+            default:
+                return false;
+        }
+    }
 }
